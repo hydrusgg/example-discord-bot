ea317dcce49b422768ed039b95f95e2750d85053
Jester <jesteriruka@gmail.com>
2022-03-05 17:26:13 -0300

    Return OK, just as the default bot

--- src/discord.js
+++ src/discord.js
@@ -23,16 +23,13 @@ const commands = {
   async addrole(member, roleId) {
     const role = await member.guild.roles.fetch(roleId)
     await member.roles.add(role, 'Hydrus')
-    return 'Role added to member'
   },
   async delrole(member, roleId) {
     const role = await member.guild.roles.fetch(roleId)
     await member.roles.remove(role, 'Hydrus')
-    return 'Role removed from member'
   },
   async ban(member) {
     await member.ban()
-    return 'Member banned'
   },
 }
 
@@ -47,19 +44,18 @@ async function work() {
   while (data = queue.shift()) {
     try {
       const [name, userId, ...args] = data.command.split(' ')
-      let message = ''
 
       const guild = await bot.guilds.fetch(process.env.GUILD_ID)
       const member = await guild.members.fetch(userId)
 
       if (name in commands) {
         console.log(`Executing: ${data.command} #${data.id}`)
-        message = await commands[name](member, ...args)
+        await commands[name](member, ...args)
       } else {
         throw new Error(`Command ${name} not found`)
       }
 
-      await api.patch('/commands/'+data.id, { status: 'done', message }).catch(err => {
+      await api.patch('/commands/'+data.id, { status: 'done', message: 'OK' }).catch(err => {
         console.error('Error at command (done) update, this may cause major issues.')
         console.error(`Command: ${data.id} (${err.message})`)
       })
